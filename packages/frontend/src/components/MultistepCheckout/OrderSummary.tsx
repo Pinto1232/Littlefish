c00c72e1618c8b3ce2e288992ed0f81855c588f3
Pinto Manuel <pintoydc@gmail.com>
2024-05-19 19:47:51 +0200

    Multi step fix added

--- packages/frontend/src/components/MultistepCheckout/OrderSummary.tsx
+++ packages/frontend/src/components/MultistepCheckout/OrderSummary.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import { Typography } from '@mui/material';
+import React from "react";
+import { Typography, Box, Button } from "@mui/material";
 
-const OrderSummary: React.FC = () => {
+interface OrderSummaryProps {
+  handleNext: () => void;
+  handleBack: () => void;
+}
+
+const OrderSummary: React.FC<OrderSummaryProps> = ({
+  handleNext,
+  handleBack,
+}) => {
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
         Order Summary
       </Typography>
-      {/* Add your order summary details here */}
+      <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 3 }}>
+        <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+          Back
+        </Button>
+        <Button
+          variant="contained"
+          onClick={handleNext}
+          sx={{ mt: 3, ml: 1, bgcolor: "#000" }}
+        >
+          Next
+        </Button>
+      </Box>
     </React.Fragment>
   );
 };
